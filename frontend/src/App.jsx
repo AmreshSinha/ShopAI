185e5532301f575861023ceb9059ddc4764a28c3
Kunalpal216 <kunalpal2115@gmail.com>
2023-08-15 23:20:02 +0530

    backup new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { Navbar } from './components/common/navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomeScreen } from './pages/homeScreen';
 
 function App() {
 
   return (
-    <>
-    <div className='app-wrapper'>
-
-    </div>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={HomeScreen} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
